1fbba6b73645b09121e234aaa7ffe287be5597fb
quangg1 <89339772+quangg1@users.noreply.github.com>
2025-05-30 05:57:57 +0700

    fix: update wsgi.py and improve X server handling

--- front-end-learning-app/wsgi.py
+++ front-end-learning-app/wsgi.py
@@ -1,1 +1,12 @@
- 
+import flet as ft
+import main
+
+def create_app():
+    page = ft.Page(
+        port=10000,
+        view=ft.WEB_BROWSER
+    )
+    main.main(page)
+    return page.web_server
+
+app = create_app() 
